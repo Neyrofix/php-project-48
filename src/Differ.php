c4a0228b6e326f01904a5efd3fe440bbd6fd32ca
dsedra <neyrofix@gmail.com>
2025-03-26 15:11:54 +0500

    refactored

--- src/Differ.php
+++ src/Differ.php
@@ -2,13 +2,23 @@
 
 namespace Hexlet\Code\Differ;
 
-function readFiles($arg1, $arg2)
+function normalizePath($path)
 {
-    $file1 = file_get_contents($arg1);
-    $file2 = file_get_contents($arg2);
-    $json1 = json_decode($file1, true);
-    $json2 = json_decode($file2, true);
-    return [$json1, $json2];
+    // Если путь абсолютный, возвращаем его как есть
+    if (strpos($path, '/') === 0) {
+        return $path;
+    }
+    // Иначе преобразуем относительный путь в абсолютный
+    return realpath(getcwd() . '/' . $path);
+}
+
+function readFile(string $filePath)
+{
+    if (!file_exists($filePath)) {
+        throw new \Exception("The file {$filePath} does not exists.");
+    }
+    $file = file_get_contents($filePath);
+    return json_decode($file, true);
 }
 
 function differ($arr1, $arr2)
@@ -37,12 +47,23 @@ function differ($arr1, $arr2)
     $result = array_filter(array_merge($mainDiff, $addedDiff));
 
     usort($result, function ($a, $b) {
-        if (substr($a, 2, 6) === substr($b, 2, 6)) {
-            if (str_starts_with($a, '-')) {
+        $chunks1 = explode(':', $a)[0];
+        $chunks2 = explode(':', $b)[0];
+        if (substr($chunks1, 2) === substr($chunks2, 2)) {
+            if (str_starts_with($chunks1, '-')) {
                 return -1;
+            } else {
+                return 1;
             }
         }
         return $a[2] < $b[2] ? -1 : 1;
     });
     return implode("\n", $result);
+}
+
+function genDiff($path1, $path2)
+{
+    $file1 = readFile(normalizePath($path1));
+    $file2 = readFile(normalizePath($path2));
+    return differ($file1, $file2);
 }
